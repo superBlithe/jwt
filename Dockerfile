1eadd076ff70f5465074bf0828a0f50e99096c1d
zhangchao <zhangchao@duiba.com.cn>
2019-12-19 00:14:54 +0800

    week

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:10.15
+LABEL maintainer "54421573@qq.com"
+COPY . /app/
+WORKDIR /app
+RUN npm install
+EXPOSE 4444
+CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use(
   })
 );
 
+router.get("/", ctx => {
+  ctx.body = "hello";
+});
 router.get("/userInfo", ctx => {
   ctx.body = {
     uid: 1,
@@ -41,7 +44,7 @@ router.post("/login", (ctx, next) => {
   } else {
     // 登录验证
     const token = jsonwebtoken.sign({ nickname: data.nickname }, "token", {
-      expiresIn: '1h'
+      expiresIn: "1h"
     });
     ctx.body = {
       success: true,
